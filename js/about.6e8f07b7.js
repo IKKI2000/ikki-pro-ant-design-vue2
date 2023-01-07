6a33e8ec38f2b34e855a146a5835353e5269d60c
IKKI2000 <IKKI2000@users.noreply.github.com>
2023-01-07 03:17:08 +0000

    【初始化】项目入口文件~

--- js/about.6e8f07b7.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkikki_pro_ant_design_vue2"]=self["webpackChunkikki_pro_ant_design_vue2"]||[]).push([[443],{6802:function(n,t,u){u.r(t),u.d(t,{default:function(){return _}});var e=function(){var n=this;n._self._c;return n._m(0)},i=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],s=u(1001),a={},r=(0,s.Z)(a,e,i,!1,null,null,null),_=r.exports}}]);
-//# sourceMappingURL=about.6e8f07b7.js.map
