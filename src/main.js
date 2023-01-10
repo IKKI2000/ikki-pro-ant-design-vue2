4823e1a8691f0610a4d8b51c8aec22952295eec2
IKKI2000 <ikki2002@qq.com>
2023-01-10 09:34:04 +0800

    【初始化】项目路径别名~

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
-import App from './ikki.vue';
-import router from './router';
-import store from './store';
+import App from '@/ikki.vue';
+import router from '@/router';
+import store from '@/store';
 
 Vue.config.productionTip = false;
 
