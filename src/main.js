146e8d98fc3bbd5a5efa38065a4549138385ab20
IKKI2000 <ikki2002@qq.com>
2023-01-12 15:40:48 +0800

    【初始化】项目主组件导入更名~

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import App from '@/ikki.vue';
+import ikki from '@/ikki.vue';
 import router from '@/router';
 import store from '@/store';
 
@@ -8,5 +8,5 @@ Vue.config.productionTip = false;
 new Vue({
     router,
     store,
-    render: (h) => h(App),
+    render: (h) => h(ikki),
 }).$mount('#app');
