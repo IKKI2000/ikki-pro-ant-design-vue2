4b7b6a3f79d35fedbd71aef5f9d8cf810a088236
IKKI2000 <ikki2002@qq.com>
2023-02-09 20:49:34 +0800

    【新增】整合 UI 框架国际化和 I18N 多语言支持~

--- src/ikki.vue
+++ src/ikki.vue
@@ -1,11 +1,13 @@
 <template>
-    <div id="app">
-        <nav>
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-        </nav>
-        <router-view />
-    </div>
+    <a-config-provider :locale="this.$i18n.messages[this.$i18n.locale]">
+        <div id="app">
+            <nav>
+                <router-link to="/">Home</router-link> |
+                <router-link to="/about">About</router-link>
+            </nav>
+            <router-view />
+        </div>
+    </a-config-provider>
 </template>
 
 <script>
--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
+import AntDesignVue_zh_CN from 'ant-design-vue/es/locale/zh_CN';
+import AntDesignVue_en_US from 'ant-design-vue/es/locale/en_US';
 
 Vue.use(VueI18n);
 
@@ -7,8 +9,8 @@ const i18n = new VueI18n({
     locale: process.env.VUE_APP_I18N_LOCALE || 'zh-CN',
     fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en-US',
     messages: {
-        'zh-CN': require('@/locales/zh-CN.json'),
-        'en-US': require('@/locales/en-US.json'),
+        'zh-CN': { ...require('@/locales/zh-CN.json'), ...AntDesignVue_zh_CN },
+        'en-US': { ...require('@/locales/en-US.json'), ...AntDesignVue_en_US },
     },
 });
 
